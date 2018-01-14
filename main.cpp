f280f36ca6ab6d01189b39c31156b38c9b7b69b7
sdrekeen <35377470+sdrekeen@users.noreply.github.com>
2018-01-14 19:29:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/video/background_segm.hpp>
 #include <iostream>
 #include <sstream>
 using namespace cv;
